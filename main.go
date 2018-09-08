b5b4c357980f1485f6d6d44a8c5030a7309124ca
Matt McKeon <matt@mattmckeon.com>
2018-09-08 11:49:34 -0400

    moved to predictions and added tests

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sort"
 	"time"
 
 	"github.com/dghubble/sling"
@@ -14,25 +13,11 @@ import (
 
 const baseURL = "https://api-v3.mbta.com/"
 
-type ScheduleParams struct {
-	MinTime string `url:"filter[min_time],omitempty"`
-	MaxTime string `url:"filter[max_time],omitempty"`
-	Stop    string `url:"filter[stop],omitempty"`
-	Include string `url:"include,omitempty"`
-}
-
-type RouteProperties struct {
-	Color       string `json:"color"`
-	TextColor   string `json:"text_color"`
-	Description string `json:"description"`
-	LongName    string `json:"long_name"`
-	Type        int    `json:"type"`
-}
-
-type DepartureResponse struct {
-	Departures []struct {
+type ApiV3Response struct {
+	Data []struct {
 		Attributes struct {
 			DepartureTime string `json:"departure_time"`
+			Status string `json:"status"`
 		} `json:"attributes"`
 		Relationships struct {
 			Route struct {
@@ -40,53 +25,112 @@ type DepartureResponse struct {
 					Id string `json:"id"`
 				} `json:"data"`
 			} `json:"route"`
+			Stop struct {
+				Data struct {
+					Id string `json:"id"`
+				} `json:"data"`
+			} `json:"stop"`
 		} `json:"relationships"`
+		Id string `json:"id"`
 	} `json:"data"`
-	RouteIndex []struct {
-		Route RouteProperties `json:"attributes"`
+	Included []struct {
+		Attributes struct {
+			// This is pretty hacky - we're basically merging the two
+			// different types we could be getting back in this list
+			// (routes and stops). Go has great support for parsing
+			// of well-formed and strongly typed JSON object graphs,
+			// and OK support for generic handling of simple and shallow
+			// JSON responses. Things get hairy though when we have deeply
+			// nested polymorphic trees like this one - you need to cast
+			// at each step of the path. There are several JSONpath
+			// libraries but they seem to be focused on fetching single
+			// values, and are a poor fit for this sort of thing where
+			// we're trying to cross-reference relationships.
+			// I tried a couple of different approaches and this seemed the
+			// least bad.
+			LongName string `json:"long_name"`
+			PlatformCode string `json:"platform_code"`
+			Type int `json:"type"`
+		} `json:"attributes"`
+		Type string `json:"type"`
 		Id string `json:"id"`
 	} `json:"included"`
 }
 
+type Params struct {
+	MinTime string `url:"filter[min_time],omitempty"`
+	MaxTime string `url:"filter[max_time],omitempty"`
+	Stop    string `url:"filter[stop],omitempty"`
+	Include string `url:"include,omitempty"`
+	Sort string `url:"sort,omitempty"`
+}
+
 type Departure struct {
-	Time time.Time
 	TimeLabel string
-	Route RouteProperties
+	Destination string
+	Track string
+	Status string
 }
 
 type MbtaService struct {
 	sling *sling.Sling
+	client *http.Client
 }
 
-func NewMbtaService(httpClient *http.Client) *MbtaService {
-	return &MbtaService{
-		sling: sling.New().Client(httpClient).Base(baseURL),
-	}
-}
-
-func (s *MbtaService) ListDepartures(params *ScheduleParams) (DepartureResponse, *http.Response, error) {
-	departures := new(DepartureResponse)
-	sling := s.sling.New().Path("schedules").QueryStruct(params)
-	req, err := sling.Request()
-	fmt.Printf("request: %v", req)
-	resp, err := sling.ReceiveSuccess(departures)
-	return *departures, resp, err
-}
-
-type Client struct {
-	MbtaService *MbtaService
-}
-
-func NewClient(httpClient *http.Client) *Client {
-	return &Client{
-		MbtaService: NewMbtaService(httpClient),
-	}
-}
-
-func NewHttpClient() *http.Client {
-	return &http.Client{
+func NewMbtaService() *MbtaService {
+	httpClient := &http.Client{
 		Timeout: time.Second * 10,
 	}
+	return &MbtaService{
+		sling: sling.New().Client(httpClient).Base(baseURL),
+		client: httpClient,
+	}
+}
+
+func ExtractDepartures(apiResponse *ApiV3Response) ([]Departure, error) {
+	trackIndex := make(map[string]string)
+	destinationIndex := make(map[string]string)
+	for _, entry := range apiResponse.Included {
+		// Only index destinations on commuter rail routes (type == 2)
+		if entry.Type == "route" && entry.Attributes.Type == 2 {
+			destinationIndex[entry.Id] = entry.Attributes.LongName
+		}
+		if entry.Type == "stop" {
+			trackIndex[entry.Id] = entry.Attributes.PlatformCode
+		}
+	}
+
+	departures := []Departure{}
+	for _, result := range apiResponse.Data {
+		// Don't show trains for which we don't have a prediction.
+		if (result.Attributes.DepartureTime != "") {
+			// Our destination index only includes commuter rail trains;
+			// we can skip anything that isn't in the index (e.g. green line etc)
+			if destination, ok := destinationIndex[result.Relationships.Route.Data.Id]; ok {
+				d := Departure{}
+				d.Destination = destination
+				// TODO: handle time parse error
+				t, _ := time.Parse(time.RFC3339, result.Attributes.DepartureTime)
+				d.TimeLabel = t.Format("3:04PM")
+				d.Status = result.Attributes.Status
+				d.Track = trackIndex[result.Relationships.Stop.Data.Id]
+				departures = append(departures, d)
+			}
+		}
+	}
+	return departures, nil
+}
+
+func (s *MbtaService) ListDepartures(params *Params) ([]Departure, *http.Response, error) {
+	sling := s.sling.New().Path("predictions").QueryStruct(params)
+	// TODO: handle request error
+	req, err := sling.Request()
+	fmt.Printf("request: %v", req)
+	apiResponse := new(ApiV3Response)
+	// TODO: handle response error
+	resp, err := sling.ReceiveSuccess(apiResponse)
+	departures, err := ExtractDepartures(apiResponse)
+	return departures, resp, err
 }
 
 func main() {
@@ -101,44 +145,17 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	// TODO: use predictions API	
-    // example: https://api-v3.mbta.com/predictions?filter%5Bmax_time%5D=09%3A13&filter%5Bmin_time%5D=08%3A43&filter%5Bstop%5D=place-north&include=route,stop
-	// included stop has platform: {"attributes": {"platform_code":"6"}, id "North Station-06"}
-	// Prediction: {"attributes": {"departure_time":"XYZ", "status":"All aboard"}}
-	// Relationships: route, stop
 	router.GET("/", func(c *gin.Context) {
-		now := time.Now()
-		params := &ScheduleParams{
-			MinTime: now.Format("15:04"),
-			MaxTime: now.Add(time.Minute * 30).Format("15:04"),
+		params := &Params{
 			Stop:    "place-north",
-			Include: "route"}
-		client := NewClient(NewHttpClient())
+			Include: "route,stop",
+			Sort: "departure_time",
+		}
+		client := NewMbtaService()
 		// TODO: handle request error
-		departureResponse, _, _ := client.MbtaService.ListDepartures(params)
-		routeIndex := make(map[string]RouteProperties)
-		for _, entry := range departureResponse.RouteIndex {
-			routeIndex[entry.Id] = entry.Route
-		}
-		nextDepartures := []Departure{}
-		for _, dr := range departureResponse.Departures {
-			routeId := dr.Relationships.Route.Data.Id
-			// TODO: make the type a constant
-			if routeIndex[routeId].Type == 2 {
-				d := Departure{}
-				// TODO: handle time parse error
-				d.Time, _ = time.Parse(time.RFC3339, dr.Attributes.DepartureTime)
-				d.TimeLabel = d.Time.Format("3:04PM")
-				d.Route = routeIndex[routeId]
-				nextDepartures = append(nextDepartures, d)
-			}
-		}
-		sort.Slice(nextDepartures, func(i, j int) bool {
-			return nextDepartures[i].Time.Before(nextDepartures[j].Time)
-		})
-		
+		departures, _, _ := client.ListDepartures(params)
 		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
-			"departures": nextDepartures,
+			"departures": departures,
 		})
 	})
 
